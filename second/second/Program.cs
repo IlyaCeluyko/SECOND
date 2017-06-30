d33337d35d8feb661da85a5c4985fe5971e80eae
Илья <ilya_celuyko@mail.ru>
2017-06-30 08:49:03 +0300

    условие

--- second/second/Program.cs
+++ second/second/Program.cs
@@ -10,6 +10,15 @@ namespace second
     {
         /// <summary>
         /// Главная точка входа для приложения.
+        /// Разработать программу на с# WinFofms с кнопкой "Загрузить",
+        /// по нажатию на которую выводится имя первого студента.
+        /// Использовать  XML файл следующего содержания
+        /// <?xml version = "1.0" encoding = "utf-8">
+        /// <department name = "FIT">
+        /// <student name = "Ольга" group ="1" age = "18" />
+        /// <student name = "Никита" group ="2" age = "19" />
+        /// <student name = "Павел" group ="7" age = "20" />
+        /// </department>
         /// </summary>
         [STAThread]
         static void Main()
